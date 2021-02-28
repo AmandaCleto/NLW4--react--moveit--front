f3d5bdf490cbb59f1f910dbf7c1308dbbf1162e1
Amanda Cleto <amanda.clet@gmail.com>
2021-02-27 22:34:20 -0300

    nlw first and second episode

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+//estrutura por volta de toda aplicacao, fixos em todas as paginas, repete em todas as paginas.
+
+import '../styles/global.css';
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
