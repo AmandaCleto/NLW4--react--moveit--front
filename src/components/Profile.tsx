1ad17159759aaf102e4e13e0c706a3fa9fbbda1e
AmandaCleto <amanda.clet@gmail.com>
2021-09-11 19:31:04 -0300

    changed colors

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,24 @@
 
 import { useContext } from 'react';
+import Avatar from "boring-avatars";
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
     const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
-          <img src="https://github.com/diego3g.png" alt="Diego Fernandes"/>
+
+          <Avatar
+            size={80}
+            name="Amanda Cleto"
+            variant="beam"
+            colors={["#C271B4", "#146A7C",  "#A3A948", "#EDB92E", "#CE1836"]}
+          />
 
           <div>
-            <strong>Diego Fernandes</strong>
+            <strong>Amanda Cleto</strong>
 
             <p>
               <img src="icons/level.svg" alt="level"/>
